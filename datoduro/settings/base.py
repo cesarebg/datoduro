811a7014150b20dc4f3b0bd16a09372fe5bcc830
cesar <cesar.barboza@pm.me>
2020-04-16 00:43:13 -0500

    env variables from gunicorn service

--- datoduro/settings/base.py
+++ datoduro/settings/base.py
@@ -30,7 +30,7 @@ def get_env_variable(var_name):
         error_msg = "Set the %s environment variable" % var_name
         raise ImproperlyConfigured(error_msg)
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production! (Done)
 
