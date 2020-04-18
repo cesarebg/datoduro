6361e3652d59c617dc5b3dd66d3432da09c196a5
cesar <cesar.barboza@pm.me>
2020-04-18 03:24:29 -0500

    creation of assets for static files

--- datoduro/settings/base.py
+++ datoduro/settings/base.py
@@ -16,7 +16,12 @@ from django.utils.translation import ugettext_lazy as _
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -154,17 +159,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "static"),
-# )
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
 
-WAGTAIL_SITE_NAME = 'Dato Duro'
+WAGTAIL_SITE_NAME = 'DatoDuro'
