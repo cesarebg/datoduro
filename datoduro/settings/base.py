cb8f76943557f57f417fe5c144c0c22c93550203
cbarboza <cesar.barboza@pm.me>
2022-01-24 17:25:25 -0600

    fixing static dir

--- datoduro/settings/base.py
+++ datoduro/settings/base.py
@@ -207,7 +207,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = '/media/'
